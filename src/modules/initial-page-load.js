40f37f6fee24f49924f3896318367cfdcb0ef005
ppost0 <p.post2321@yahoo.com>
2023-01-11 11:29:15 -0500

    Added base elements using JS

--- src/modules/initial-page-load.js
+++ src/modules/initial-page-load.js
@@ -10,15 +10,18 @@
 
 function loadPage() {
   const content = document.getElementById('content');
+  const background = document.createElement('img');
   const title = document.createElement('h1');
   const copy = document.createElement('h2');
 
+  background.setAttribute('id', 'background');
+  background.src = '../src/assets/imgs/food.jpg';
   title.innerText = `Jim's Tavern`;
   copy.innerText = 'The highest rated burgers this side of the Mississippi';
 
+  content.appendChild(background);
   content.appendChild(title);
   content.appendChild(copy);
-  console.log(title, copy);
 }
 
 export default loadPage;
