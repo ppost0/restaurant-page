7159b6eb9a6f81964fe84c839c89c63e98da88d9
ppost0 <p.post2321@yahoo.com>
2023-01-16 14:40:41 -0500

    Style navbar and buttons

--- src/modules/initial-page-load.js
+++ src/modules/initial-page-load.js
@@ -13,6 +13,28 @@ function createHeader(id, titleText, copyText) {
   return header;
 }
 
+function createBtn(id, text) {
+  const btn = document.createElement('button');
+  btn.setAttribute('id', id);
+  btn.textContent = text;
+  btn.classList.add('tab');
+  return btn;
+}
+
+function createNav(id) {
+  const nav = document.createElement('nav');
+  nav.setAttribute('id', id);
+
+  const homeBtn = createBtn('home', 'Home');
+  const menuBtn = createBtn('menu', 'Menu');
+  const contactBtn = createBtn('contact', 'Contact');
+
+  nav.appendChild(homeBtn);
+  nav.appendChild(menuBtn);
+  nav.appendChild(contactBtn);
+
+  return nav;
+}
 
 function createMain(id) {
   const main = document.createElement('main');
@@ -21,19 +43,17 @@ function createMain(id) {
 }
 
 
-
 function loadPage() {
   const content = document.getElementById('content');
 
   const header = createHeader('header', `Jim's Tavern`, 'The highest rated burgers this side of the Mississippi')
-
-
+  const nav = createNav('nav');
   const main = createMain('main');
 
 
   content.appendChild(header);
+  content.appendChild(nav);
   content.appendChild(main);
-  // content.appendChild(nav);
 }
 
 export default loadPage;
