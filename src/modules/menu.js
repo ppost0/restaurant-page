aea31a40822b76e4872efddc14163e45b8b756e9
ppost0 <p.post2321@yahoo.com>
2023-01-16 17:00:55 -0500

    Add several more menu items

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -35,6 +35,11 @@ function createMenu() {
     createItem('/Users/patrick/repos/restaurant-page/src/assets/imgs/item1.jpg', 'The Standard American', '$8.99', 'American Cheeseburger'),
     createItem('/Users/patrick/repos/restaurant-page/src/assets/imgs/item2.jpg',
     'The Tex Mex', '$9.99', 'Tex-Mex Cheeseburger'),
+    createItem('/Users/patrick/repos/restaurant-page/src/assets/imgs/item3.jpg', 'The Bacon Beast', '$10.99', 'Bacon Cheeseburger'),
+    createItem('/Users/patrick/repos/restaurant-page/src/assets/imgs/item4.jpg', 'The Cowboy', '$9.99', 'Onion Ring Cheeseburger'),
+    createItem('/Users/patrick/repos/restaurant-page/src/assets/imgs/item5.jpeg', 'The Hawaiian', '$8.99', 'Cheeseburger with Pineapple'),
+    createItem('/Users/patrick/repos/restaurant-page/src/assets/imgs/item6.jpg', 'The Vegetarian', '$8.99', 'Veggie Burger'),
+    createItem('/Users/patrick/repos/restaurant-page/src/assets/imgs/item7.jpeg', 'The Holy Guacamole', '$9.99', 'Guacamole Cheeseburger')
 
 
 
